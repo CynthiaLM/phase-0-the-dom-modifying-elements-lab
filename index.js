e9040320bf9025ce93ce4ede32eafa128fd30113
Laleti Cynthia <+>
2023-07-28 09:38:08 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//remove main with id of main
 const main = document.querySelector('#main');
   main.remove();
 
+  //create h1 with id of victory
 const newHeader = document.createElement ('h1');
   newHeader.id = 'victory';
-  newHeader.textContent = 'Cynthia is the champion';
+  newHeader.textContent = 'laleti is the champion';
   document.body.append(newHeader);
